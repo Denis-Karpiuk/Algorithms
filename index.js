c68fe2fbeddff8d222799006945a271b68940dd5
Dzianis Karpiuk <dzianis.karpiuk@itechart-group.com>
2021-10-05 18:09:51 +0300

    binary

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 //* Бинарный поиск
 
-const sortArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const sortArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
 
-const binarySearch = (arr, elem) => {
-	let start = 0
-	let end = arr.length - 1
-	while (start < end) {
-		let midle = Math.floor((start + end) / 2)
+function binarySearch(arr, elem) {
+	let left = 0
+	let right = arr.length - 1
+	let midle = Math.round((left + right) / 2)
+
+	console.log(left, right, midle)
+	while (left <= right) {
 		let guess = arr[midle]
-
-		if (guess === elem) return midle
-		if (guess > elem) {
-			end = midle - 1
-		} else {
-			if (guess < elem) {
-				start = midle + 1
-			}
+		if (guess === elem) {
+			return midle
+		} else if (guess > elem) {
+			right = midle - 1
+		} else if (guess < elem) {
+			left = midle + 1
 		}
 	}
-	return null
+	return -1
 }
+
+const result = binarySearch(sortArray, 2)
+console.log(result)
