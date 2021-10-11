68473a819b5a5dd9836d0fb72ed24d7d9a02f83d
Denis Karpiuk <dzianiskarpiuk@gmail.com>
2021-10-11 10:31:12 +0300

    sor O(n2)

--- index.js
+++ index.js
@@ -1,26 +1,72 @@
-//* Бинарный поиск
-
+//! Бинарный поиск O(log n)
+//* Обязательное условие, массив дожен быть отсортирован
 const sortArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
 
-function binarySearch(arr, elem) {
+function binarySearch(arr, target) {
 	let left = 0
 	let right = arr.length - 1
-	let midle = Math.round((left + right) / 2)
+	let midle
 
-	console.log(left, right, midle)
 	while (left <= right) {
+		midle = Math.floor((right + left) / 2)
 		let guess = arr[midle]
 
-		if (guess === elem) {
-			return midle
-		} else if (guess > elem) {
-			right = midle - 1
-		} else if (guess < elem) {
-			left = midle + 1
-		}
+		if (guess > target) right = midle - 1
+		if (guess < target) left = midle + 1
+		if (guess === target) return midle
 	}
-	return -1
+	return null
+}
+//! Сортировка выбором O(n2)
+const numbersArray = [3, 7, 9, 8, 2]
+function smalestItem(array) {
+	let item = array[0]
+	for (let i = 0; i < array.length; i++) {
+		const element = array[i]
+		if (element < item) item = element
+	}
+	return item
 }
 
-const result = binarySearch(sortArray, 2)
-console.log(result)
+function sotrArray(arr) {
+	let array = arr
+	let newArr = []
+	for (let i = 0; i < array.length; i++) {
+		// дописать код
+	}
+	return newArr
+}
+
+console.log(sotrArray(numbersArray))
+
+//! =====Рекурсия=====
+//*Поиск ключа -  Обычный цикл
+let boxes = ['notKey', 'notKey', 'notKey', ['key']]
+function searchKey(boxes) {
+	let allBoxes = [...boxes]
+	for (let i = 0; i < allBoxes.length; i++) {
+		if (allBoxes[i] === 'key') {
+			return 'key found'
+		} else if (Array.isArray(allBoxes[i])) {
+			allBoxes.push(allBoxes[i][0])
+		}
+	}
+	return 'not key'
+}
+
+//*Поиск ключа - Рекурсия
+function searchKeyRec(box) {
+	for (let i = 0; i < box.length; i++) {
+		if (Array.isArray(box[i])) {
+			searchKeyRec(box[i])
+		} else if (box[i] === 'key') {
+			console.log('key found')
+		}
+	}
+}
+
+//* Факториал - рекурсия.
+function fact(num) {
+	if (num === 1) return 1
+	return num * fact(num - 1)
+}
